61dde2407e7a9f98b4b9334dea61f89f3b4d2347
antocecere <antocecere77@gmail.com>
2020-04-28 10:06:17 +0200

    Nextjs & component & pages

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import User from '../../components/User';
 
 const authIndexPage = () => (
     <div>
         <h1>The Auth Index Page</h1>
+        <User name="Antonio" age={43}/>
     </div>
 );
 
